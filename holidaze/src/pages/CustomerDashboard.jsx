f4dfe861e2d6082e928649b0713711bae5e1505e
ellie489 <114486930+ellie489@users.noreply.github.com>
2024-05-21 11:51:46 +0200

    Adds dashboards for users

---
+++ holidaze/src/pages/CustomerDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CustomerDashboard = () => {
+    return (
+        <div>
+            <h1>Customer Dashboard</h1>
+            <p>Welcome to your dashboard!</p>
+        </div>
+    );
+};
+
+export default CustomerDashboard;
---
+++ holidaze/src/pages/ManagerDashboard.jsx
@@ -0,0 +1,13 @@
+// src/pages/ManagerDashboard.jsx
+import React from 'react';
+
+const ManagerDashboard = () => {
+    return (
+        <div>
+            <h1>Manager Dashboard</h1>
+            <p>Welcome to your dashboard!</p>
+        </div>
+    );
+};
+
+export default ManagerDashboard;
