8e21725617bbb1a22475da89c01018e2b42a5bf5
ellie489 <114486930+ellie489@users.noreply.github.com>
2024-05-21 11:52:50 +0200

    Updates routes

--- holidaze/src/components/App/index.jsx
+++ holidaze/src/components/App/index.jsx
@@ -7,6 +7,9 @@ import RegisterAsCustomer from '../Auth/RegisterAsCustomer';
 import RegisterAsManager from '../Auth/RegisterAsManager';
 import Header from '../Layout/Header';
 import Footer from '../Layout/Footer';
+import CustomerDashboard from '../../pages/CustomerDashboard';
+import ManagerDashboard from '../../pages/ManagerDashboard';
+import Login from '../Auth/Login';
 
 const App = () => {
     return (
@@ -19,6 +22,9 @@ const App = () => {
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/register-customer" element={<RegisterAsCustomer />} />
                     <Route path="/register-manager" element={<RegisterAsManager />} />
+                    <Route path="/customer-dashboard" element={<CustomerDashboard />} />
+                    <Route path="/manager-dashboard" element={<ManagerDashboard />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
             </div>
             <Footer />
