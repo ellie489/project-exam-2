c96629326fec73079fc4663b49b1f7f0cdb3469e
ellie489 <114486930+ellie489@users.noreply.github.com>
2024-05-21 11:31:16 +0200

    Adds Routing and pages to app

--- holidaze/src/components/App/index.jsx
+++ holidaze/src/components/App/index.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../../pages/Home';
+import About from '../../pages/About';
+import Contact from '../../pages/Contact';
+import RegisterAsCustomer from '../Auth/RegisterAsCustomer';
+import RegisterAsManager from '../Auth/RegisterAsManager';
+import Header from '../Layout/Header';
+import Footer from '../Layout/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Header />
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/register-customer" element={<RegisterAsCustomer />} />
+                    <Route path="/register-manager" element={<RegisterAsManager />} />
+                </Routes>
+            </div>
+            <Footer />
+        </Router>
+    );
+};
 
 export default App;
