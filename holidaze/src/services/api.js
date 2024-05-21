6d499ac2e7202724bb4c5a3ecc341994ca2e25e6
ellie489 <114486930+ellie489@users.noreply.github.com>
2024-05-21 11:30:43 +0200

    function to register user

---
+++ holidaze/src/services/api.js
@@ -0,0 +1,21 @@
+import { API_BASE_URL } from '../constants/config';
+
+export const registerUser = async (user) => {
+  const response = await fetch(`${API_BASE_URL}auth/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+
+  if (!response.ok) {
+    const errorResponse = await response.json();
+    const errorMessage = errorResponse.errors
+      ? errorResponse.errors.map((err) => err.message).join(', ')
+      : 'Unknown error occurred';
+    throw new Error(errorMessage);
+  }
+
+  return response.json();
+};
